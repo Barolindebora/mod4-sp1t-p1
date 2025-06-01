7993bb94c6840cb6e8becf3e88af02a33db720bb
Debora Ines Barolin <barolindebora224@gmail.com>
2025-06-01 17:49:25 -0300

    estilos al navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,13 +6,13 @@ import nuezParaLogo from "../assets/nuezParaLogo.png"
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
-  const toggleMenu = () => setIsOpen(!isOpen);
+ 
 
   return (
 
     <nav className="w-full bg-amber-200 text-brown-900 shadow-md">
 
-      {/* vista de escritorio */}
+      {/* vista pc de escritorio------------------------------------------------------------------------------------------------------------*/}
       <div className="flex justify-between items-center sm:px-12 sm:py-3 px-4 py-2">
 
         {/* LOGO */}
@@ -21,24 +21,24 @@ const Navbar = () => {
         
             srcSet={nuezParaLogo}
             alt="logo nueces"
-            className="w-[50px]"
+            className="w-[40px]"
           />
-          <p className="font-bold text-2xl">Nueces LOLO</p>
+          <p className="font-bold text-2xl"><span className="text-amber-600 text-shadow-4xs" >Nueces</span>LOLO</p>
         </div>
 
         {/* BOTÓN HAMBURGUESA (celular) */}
-        <button className="md:hidden p-2" onClick={toggleMenu}>
+        <button className="md:hidden p-2" onClick={() => setIsOpen(!isOpen)}>
           <i className={`bi ${isOpen ? 'bi-x-lg' : 'bi-list'} text-2xl`}></i>
         </button>
 
-        {/* LINKS NAV + REDES (modo pc escritorio) */}
+        {/* LINKS de la lista de navegacion + iconos de redes (modo pc escritorio) */}
         <div className="hidden md:flex items-center gap-8">
           <ul className="flex space-x-4">
             {linksMenu.map((link) => (
               <li key={link.id}>
                 <a
                   href={link.link}
-                  className="text-sm hover:text-yellow-500 transition hover:scale-110 inline-block"
+                  className="text-sm hover:text-amber-600 transition hover:scale-110 inline-block"
                 >
                   {link.name}
                 </a>
@@ -53,7 +53,7 @@ const Navbar = () => {
                   href={link.link}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="hover:text-yellow-500 transition hover:scale-125"
+                  className="hover:text-amber-600 transition hover:scale-125"
                 >
                   <i className={`${link.icon} text-lg`}></i>
                 </a>
@@ -63,17 +63,23 @@ const Navbar = () => {
         </div>
       </div>
 
-      {/* vista para telefonos celulares */}
-      <div className={`md:hidden bg-gray-800 overflow-hidden transition-all duration-300 ${isOpen ? 'max-h-screen opacity-100' : 'max-h-0 opacity-0'}`}>
+      
+      
+      
+      {/* vista para telefonos celulares -----------------------------------------------------------------------------------------------------*/}
+      
+      
+      
+      <div className={`md:hidden bg-amber-100 overflow-hidden transition-all duration-300 ${isOpen ? 'max-h-screen opacity-100' : 'max-h-0 opacity-0'}`}>
         <ul className="flex flex-col px-4 py-2">
           {linksMenu.map((link) => (
             <li key={link.id} className="py-2 text-center">
               <a
                 href={link.link}
-                className="text-sm hover:text-yellow-500 transition hover:scale-110 inline-block"
+                className="text-sm hover:text-amber-600 transition hover:scale-110 inline-block"
                 onClick={() => setIsOpen(false)}
               >
-                {link.title}
+                {link.name}
               </a>
             </li>
           ))}
@@ -84,9 +90,9 @@ const Navbar = () => {
             <li key={link.id}>
               <a
                 href={link.link}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="hover:text-yellow-500"
+                target="_blank"//Indica que el enlace se debe abrir en una nueva pestaña del navegador.
+                rel="noopener noreferrer"//noreferrer Evita que se envíe información de referencia (como la URL de tu sitio) al destino
+                className="hover:text-amber-600 transition hover:scale-125"
                 onClick={() => setIsOpen(false)}
               >
                 <i className={`${link.icon} text-lg`}></i>
