38fe8be73bcb319bcb0b9b179cb3156f8570182c
Debora Ines Barolin <barolindebora224@gmail.com>
2025-06-03 20:20:50 -0300

    tarjetas como componentes

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,16 +1,21 @@
 
 
-import { motion } from 'framer-motion';
-import { useState } from 'react';
+
 import loloNuez from '../assets/loloNuez.png';
 import nuecesia from '../assets/nuecesia.jpg';
 import nuecesSueltas from '../assets/nuecesSueltas.jpg';
 import confitadas from '../assets/confitadas.jpg';
-import nuezParaLogo from '../assets/nuezParaLogo.png';
+import Tarjeta from './Tarjeta';
+
 
 
 
 function Body() {
+  const tarjetas=[
+    {id: 1, imagen: nuecesSueltas, texto: 'Nueces por KG', descripcion: 'Paquetes por 1/4KG - 1/2KG - 1KG'},
+    {id: 2, imagen: confitadas, texto: 'Nueces Confitadas', descripcion: 'Deliciosas nueces rellenas con dulce de leche cubiertas de azúcar.'},
+    {id: 3, imagen: loloNuez, texto: 'Ordenar', descripcion: 'Quiero mis nueces!!'}
+  ]
   return (
     <body>
       <div className="w-full h-screen relative overflow-hidden">
@@ -26,36 +31,15 @@ function Body() {
         />
         {/* Tarjetas */}
         <div className="absolute inset-0 mt-10  flex flex-col md:flex-row justify-center items-center gap-6 p-6">
-          {/* Tarjeta 1 */}
-          <div className="bg-[#3b2f2f]/20 backdrop-blur-md rounded-xl shadow-lg p-6 w-72 text-center text-white hover:scale-105 transition-transform">
-            <img src={nuecesSueltas} alt="Nueces Sueltas" className="w-24 h-24 mx-auto mb-4 rounded-full" />
-            <h2 className="text-xl font-bold mb-2">Nueces por KG</h2>
-            <p className="text-sm">Paquetes por 1/4KG - 1/2KG - 1KG</p>
-          </div>
-          {/* Tarjeta 2 */}
-          <div className="bg-[#3b2f2f]/20 backdrop-blur-md rounded-xl shadow-lg p-6 w-72 text-center text-white hover:scale-105 transition-transform">
-            <img src={confitadas} alt="Nueces Confitadas" className="w-24 h-24 mx-auto mb-4 rounded-full" />
-            <h2 className="text-xl font-bold mb-2">Nueces Confitadas</h2>
-            <p className="text-sm">Deliciosas nueces rellenas con dulce de leche  cubiertas de azúcar.</p>
-          </div>
-          {/* Tarjeta 3 */}
-          <div className="bg-[#3b2f2f]/20 backdrop-blur-md rounded-xl shadow-lg p-6 w-72 text-center text-white hover:scale-105 transition-transform">
-            <img src={loloNuez} alt="Ordenar" className="w-24 h-24 mx-auto mb-4 rounded-full" />
-            <h2 className="text-xl font-bold mb-2">Ordenar</h2>
-            <a
-              href="https://wa.me/549XXXXXXXXXX?text=Hola,%20quiero%20mis%20nueces!"
-              target="_blank"
-              rel="noopener noreferrer"
-              className=" rounded-md p-2 border-white border-2 inline-flex items-center gap-2"
-            >
-              <i className="bi bi-whatsapp"></i>
-              <span className="text-l">Quiero mis nueces!!</span>
-            </a>
-          </div>
-          
-
+          {tarjetas.map((tarjeta) => (
+            <Tarjeta
+              key={tarjeta.id}
+              imagen={tarjeta.imagen}
+              texto={tarjeta.texto}
+              descripcion={tarjeta.descripcion}
+            />
+          ))}
         </div>
-
       </div>
     </body>
   );
---
+++ src/components/Tarjeta.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Tarjeta({ imagen, texto, descripcion }) {
+  return (
+    <div className='bg-[#3b2f2f]/20 backdrop-blur-md rounded-xl shadow-lg p-6 w-72 text-center text-white hover:scale-105 transition-transform'>
+      <>
+        <img
+          src={imagen}
+          alt="imagen"
+          className="w-24 h-24 mx-auto mb-4 rounded-full"
+        />
+        <h2 className="text-xl font-bold mb-2">{texto}</h2>
+        <p className="text-sm">{descripcion}</p>
+        
+      </>
+    </div>
+  )
+}
+
+export default Tarjeta
